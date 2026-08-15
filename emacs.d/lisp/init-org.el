6a27f9584f2c95897b2c1f508d534fb75bb719a6
Marc Shen <marcshen24@outlook.com>
2026-08-16 01:03:07 +0800

    Changes to be committed: 	modified:   emacs.d/lisp/init-org.el

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -6,10 +6,12 @@
   :bind (("C-c a" . org-agenda) ("C-c n" . org-capture))
   :custom
   (org-directory "~/Documents/org")
-  (org-default-notes-file (expand-file-name "inbox.org" org-directory))
-  (org-agenda-files (list (expand-file-name "inbox.org" org-directory)
-                          (expand-file-name "research.org" org-directory)
-                          (expand-file-name "tasks.org" org-directory)))
+  (org-default-notes-file (expand-file-name "notes.org" org-directory))
+  ;; One file per kind of content: tasks.org holds TODO items, calendar.org
+  ;; holds dated events, notes.org holds everything without a state or a date.
+  (org-agenda-files (list (expand-file-name "tasks.org" org-directory)
+                          (expand-file-name "calendar.org" org-directory)
+                          (expand-file-name "notes.org" org-directory)))
   (org-log-done 'time)
   (org-log-into-drawer t)
   (org-startup-indented t)
@@ -23,10 +25,15 @@
   (org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAIT(w@/!)" "|"
                                 "DONE(d!)" "CANCELLED(c@)"))))
 
+;; `%^T' prompts for an active timestamp through the calendar picker, which is
+;; what puts an event on the agenda's time grid.  Tasks deliberately capture
+;; without a date: scheduling them is a separate decision made later with
+;; `C-c C-s' / `C-c C-d'.
 (setq org-capture-templates
-      '(("t" "Task" entry (file "inbox.org") "* TODO %?\n  %U\n")
-        ("r" "Research note" entry (file "research.org") "* %^{Title}\n  %U\n\n%?")
-        ("m" "Meeting note" entry (file+headline "research.org" "Meetings")
+      '(("t" "Task" entry (file "tasks.org") "* TODO %?\n  %U\n")
+        ("e" "Event" entry (file "calendar.org") "* %^{Event}\n  %^T\n\n%?")
+        ("n" "Note" entry (file "notes.org") "* %^{Title}\n  %U\n\n%?")
+        ("m" "Meeting note" entry (file+headline "notes.org" "Meetings")
          "* %^{Meeting}\n  %U\n\n%?")))
 
 (use-package org-modern
